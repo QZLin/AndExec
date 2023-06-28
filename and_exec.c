4ab9e778bffa184a0c8bbe3f22356130146ba83f
QZLin <qzlin01@163.com>
2023-06-28 15:51:00 +0800

    use msvc args parser

--- and_exec.c
+++ and_exec.c
@@ -11,18 +11,7 @@ LPWSTR subString(LPWSTR str, UINT64 startIndex, UINT64 endIndex) {
     return result;
 }
 
-bool validArgChar(LPWSTR str, WCHAR character, UINT64 index) {
-    if (str[index] == character) {
-        if (index == 0) return true;
-    } else return false;
-    if (validArgChar(str, '\\', index - 1))
-        return false;
-    return true;
-}
 
-// andexec :commandline
-// andexec :commandline:workdir
-// andexec :[flags]:commandline:workdir
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR pCmdLine, int nCmdShow) {
     LPWSTR commandLine = NULL;
     LPWSTR workingDir = NULL;
@@ -58,60 +47,6 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR pCmdLin
             exit(3);
     }
 
-
-//    LPWSTR test1 = L"0123456789";
-//    LPWSTR r1 = subString(test1, 0, 1);
-
-    /*UINT64 cmdLineLen = wcslen(pCmdLine);
-    if (cmdLineLen == 0)
-        exit(2);
-
-    const WCHAR SEP = pCmdLine[0];
-
-    int sepCount = 0;
-    int sepLocMaxi = 10;
-    UINT64 *sepLocs = malloc(sepLocMaxi * sizeof(UINT64));
-    for (UINT64 i = 0; i < cmdLineLen; i++) {
-        if (pCmdLine[i] != SEP)
-            continue;
-        if (sepCount < sepLocMaxi)
-            sepLocs[sepCount] = i;
-        else {
-            sepLocMaxi += 10;
-            realloc(sepLocs, sepLocMaxi * sizeof(UINT64));
-            sepLocs[sepCount] = i;
-        }
-        sepCount++;
-    }*/
-
-
-    /*switch (sepCount) {
-        case 0:
-            commandLine = pCmdLine;
-            break;
-        case 1:
-            commandLine = subString(pCmdLine, 1, sepLocs[0]);
-        case 2:
-            commandLine = subString(pCmdLine, 0, sepLocs[0]);
-            workingDir = subString(pCmdLine, sepLocs[1] + 1, cmdLineLen);
-            break;
-        case 3:
-            LPWSTR flags = subString(pCmdLine, 0, sepLocs[0]);
-            UINT64 flagsLen = wcslen(flags);
-            for (UINT64 i = 0; i < flagsLen; i++) {
-                switch (flags[i]) {
-                    case FLAGWaitExit:
-                        waitExit = TRUE;
-                        break;
-                }
-
-            }
-            commandLine = subString(pCmdLine, sepLocs[0] + 1, sepLocs[1]);
-            workingDir = subString(pCmdLine, sepLocs[1] + 1, cmdLineLen);
-            break;
-        default:
-            break;
-    }*/
 
     STARTUPINFOW si;
     PROCESS_INFORMATION pi;
